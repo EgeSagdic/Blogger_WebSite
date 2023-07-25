16697d73ed20679c6eef5726f9372f26d76d5038
egesa <egesa@LAPTOP-FEBL0K35>
2023-07-26 00:21:46 +0300

    Add project files.

---
+++ Blogger_WebSite/Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blogger_WebSite.Models
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Bloggers> Bloggers { get; set; }
+
+        public DbSet<Categories> Categories { get; set; }
+
+        public DbSet<Contacts> Contacts { get; set; }
+
+        public DbSet<Projects> Projects { get; set; }
+
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-FEBL0K35\SQLEXPRESS;Database=BlogDatabase;Trusted_Connection=true;encrypt=false");
+        }
+    }
+}
